66197e0a25cc602b57b5dca820c4215ba35aed06
Pedro Henrique <pedrohenrique20311@gmail.com>
2023-10-22 15:27:29 -0300

    projeto criado

---
+++ src/main/java/me/dio/santanterdevweek2023/SantanterDevWeek2023Application.java
@@ -0,0 +1,13 @@
+package me.dio.santanterdevweek2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanterDevWeek2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanterDevWeek2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santanterdevweek2023/SantanterDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santanterdevweek2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanterDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
